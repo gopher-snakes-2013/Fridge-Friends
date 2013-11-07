9030273067e7bbe1f986215bbee12cf07fd366ee
danbender <=>
2013-11-07 15:44:24 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FridgeFriends::Application.config.secret_key_base = 'e5e3691da8929751da1876163737a943744e027d96c21e5067157cbfb805e44726e57c64e28a685de053a65e8e11eb1ae4ea9a65534fde4061f6e8ebf3da0105'
